4dd76ff7a321461c50f76443d91c0871d2515d66
Dovran Annakuliev <dovran.annakuliev@gmail.com>
2020-03-09 15:12:13 +0300

    mg

--- errors.c
+++ errors.c
@@ -6,7 +6,7 @@
 /*   By: rfork <rfork@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 13:33:16 by rfork             #+#    #+#             */
-/*   Updated: 2020/03/09 13:33:16 by rfork            ###   ########.fr       */
+/*   Updated: 2020/03/09 14:29:19 by rfork            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,17 @@ void	errors(int err)
 	if (err == 1)
 	{
 		write(1, "usage: ./fractol [argument ...]\n", 32);
+		write(1, "\tuse argument:\tJulia\n", 21);
+		write(1, "\tuse argument:\tMandelbrot\n", 26);
+		write(1, "\tuse argument:\tDragon\n", 22);
 		exit(0);
 	}
 	else if (err == 2)
 	{
 		write(1, "invalid argument\n", 17);
+		write(1, "\tuse argument:\tJulia\n", 21);
+		write(1, "\tuse argument:\tMandelbrot\n", 26);
+		write(1, "\tuse argument:\tDragon\n", 22);
 		exit(0);
 	}
 	else
