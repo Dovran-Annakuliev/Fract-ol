a0a054f8d7b484f41bb1fca132ad349b22e707dc
Dovran Annakuliev <55741979+Dovran-Annakuliev@users.noreply.github.com>
2020-07-26 16:24:51 +0300

    mg

--- includes/key.h
+++ includes/key.h
@@ -1,26 +1,6 @@
 #ifndef KEY_H
 # define KEY_H
 
-//# define ESC 53
-//# define ZOOM_P 69
-//# define ZOOM_M 78
-//# define Z_P 67
-//# define Z_M 75
-//# define DEFAULT 71
-//# define ANGL_X_P 88
-//# define ANGL_X_M 86
-//# define ANGL_Y_P 91
-//# define ANGL_Y_M 87
-//# define ANGL_Z_P 92
-//# define ANGL_Z_M 89
-//# define X_DP_M 123
-//# define X_DP_P 124
-//# define Y_DP_P 125
-//# define Y_DP_M 126
-//# define Z_DP_P 83
-//# define Z_DP_M 82
-//# define PROJECTION 65
-
 # define W 13
 # define S 1
 # define A 0
@@ -45,8 +25,5 @@
 # define WHEEL_DOWN 5
 # define LEFT_MB 1
 # define RIGHT_MB 2
-# define AR_UP 126
-# define AR_DOWN 125
-# define AR_LEFT 123
-# define AR_RIGHT 124
+
 #endif
